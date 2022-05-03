bedebd5894e761addeef22e8498731010ddf6f39
SairamNomula <sairamnomula14@gmail.com>
2022-05-03 10:42:45 +0530

    updates

--- src/components/pages/Create/Create.js
+++ src/components/pages/Create/Create.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Hero from '../../Hero'
 import {CreateObjOne} from './Data'
+import Form from '../../Form/Form'
+import Upload from '../../Upload'
 import Footer from '../../Footer'
+
 function Create() {
     return (
         <>
             <Hero {...CreateObjOne} />
+            <Upload />
+            <Form />
             <Footer />
         </>
     )
